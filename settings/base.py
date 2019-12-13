829a83e5300b4b4e2af5d9a56afcb59144ed50a5
Naresh Kumar Jaggi <naresh.jaggi@exiverprojects.com>
2019-12-13 14:17:13 +0530

    added media and static file with intial registeration template

--- settings/base.py
+++ settings/base.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

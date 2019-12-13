90b010bd430ba994950dbd987d37dc6fd62f3761
Naresh Kumar Jaggi <naresh.jaggi@exiverprojects.com>
2019-12-13 19:36:39 +0530

    complte login, signup and logout setup without django message delivery

--- excalibur/urls.py
+++ excalibur/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('excaliburclient.urls')),
+    path('', include(('excaliburclient.urls', 'excaliburclient'), namespace='excaliburclient'), name='excaliburclient'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- excaliburclient/views.py
+++ excaliburclient/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 from django.utils.decorators import method_decorator
@@ -10,19 +11,60 @@ from excaliburclient.models import File
 
 
 class LoginView(View):
+    """
+    View Used to login to the application
+    """
+    template_name = 'excaliburclient/login.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'excaliburclient/base.html')
+        if request.user.is_authenticated and request.user.is_active:
+            return redirect('excaliburclient:home')
+        else:
+            return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('excaliburclient:home')
+        else:
+            return redirect('excaliburclient:signup')
+
 
 class LogoutView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'excaliburclient/base.html')
-   
+        logout(request)
+        return redirect('excaliburclient:login')
+
+
 class SignUpView(View):
+    template_name = 'excaliburclient/signup.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'excaliburclient/base.html')
+        if request.user.is_authenticated and request.user.is_active:
+            return redirect('excaliburclient:home')
+        else:
+            return render(request, self.template_name)
+        
+
+    def post(self, request, *args, **kwargs):
+        first_name = request.POST.get('first_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create(first_name=first_name, email=email, username=username)
+        user.set_password(password)
+        user.save()
+        # authenticating the user
+        user = authenticate(username=user.username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('excaliburclient:home')
+        else:
+            return  redirect('excaliburclient:signup')
 
 
 @method_decorator(login_required, name='dispatch')

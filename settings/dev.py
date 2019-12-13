0ff2b6601fbdcdfb4d299043e567ac9db1eeb85b
Sahil <sahiil@protonmail.com>
2019-12-13 11:32:57 +0530

    Initial commit.

---
+++ settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'excaliburport',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
